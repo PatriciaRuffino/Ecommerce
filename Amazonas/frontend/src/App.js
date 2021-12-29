959a6bbfaaabfab6cf187ef1c48af9bc5ef17e9c
PatriciaRuffino <sindelformr@gmail.com>
2021-12-29 13:44:58 -0300

    add cart

--- Amazonas/frontend/src/App.js
+++ Amazonas/frontend/src/App.js
@@ -1,5 +1,6 @@
 /* import './App.css'; */
-import { BrowserRouter, Route } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { BrowserRouter, Link, Route } from 'react-router-dom';
 import CartScreen from './screens/CartScreen';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
@@ -8,16 +9,23 @@ import ProductScreen from './screens/ProductScreen';
 
 
 function App() {
+
+    const cart = useSelector(state => state.cart);
+    const{cartItems} = cart;
     return (
         <BrowserRouter>
             <div className="grid-container">
                 <header className="row">
                     <div>
-                        <a className="brand" href="/">Amazon</a>
+                        <Link className="brand" to="/">Amazon</Link>
                     </div>
                     <div>
-                        <a href="/cart">Cart</a>
-                        <a href="/signin">Sign In</a>
+                        <Link to="/cart">Cart
+                        {cartItems.length > 0 &&(
+                        <span className="badge">{cartItems.length}</span>
+                        )}
+                        </Link>
+                        <Link to="/signin">Sign In</Link>
                     </div>
                 </header>
                 <main>
